61a71a3e683df139f974628e7c4f1c15b6106b93
Daniel Tebor <danieltebor@gmail.com>
2023-10-25 12:06:09 -0400

    Update cs4306-course-project.py

--- src/cs4306-course-project.py
+++ src/cs4306-course-project.py
@@ -1,5 +1,6 @@
 import cppyy
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Load libcs4306-course-project.dll
 cppyy.include('./src/eight_puzzle_node.hpp')
@@ -7,7 +8,7 @@ cppyy.include('./src/eight_puzzle.hpp')
 cppyy.include('./src/search_alg.hpp')
 lib = cppyy.load_library('./lib/libcs4306-course-project.dll')
 
-num_times_to_run = 100
+num_times_to_run = 10
 
 a_star_num_moves_to_goal_avg = 0
 a_star_num_nodes_visited_results = []
@@ -42,17 +43,31 @@ for i in range(0, num_times_to_run):
     multithreaded_bfs_nodes_visited_results.append(multithreaded_bfs_result.num_nodes_visited)
     multithreaded_bfs_time_taken_ms_results.append(multithreaded_bfs_result.time_taken_ms)
     
-fig, axises = plt.subplots()
+plt.plot(a_star_num_nodes_visited_results, a_star_time_taken_ms_results, 'o', label='A*')
+plt.plot(bfs_nodes_visited_results, bfs_time_taken_ms_results, 'o', label='BFS')
+plt.plot(multithreaded_bfs_nodes_visited_results, multithreaded_bfs_time_taken_ms_results, 'o', label='Multithreaded BFS')
 
-axises.plot(a_star_num_nodes_visited_results, a_star_time_taken_ms_results, label='A*')
-axises.plot(bfs_nodes_visited_results, bfs_time_taken_ms_results, label='BFS')
-axises.plot(multithreaded_bfs_nodes_visited_results, multithreaded_bfs_time_taken_ms_results, label='Multithreaded BFS')
+plt.xlabel('Number of Nodes Visited')
+plt.ylabel('Time Taken (ms)')
 
-axises.set_xlabel('Number of Nodes Visited')
-axises.set_ylabel('Time Taken (ms)')
+plt.title('Number of Nodes Visited vs Time Taken')
 
-axises.set_title('Number of Nodes Visited vs Time Taken')
+plt.legend()
 
-axises.legend()
+a_star_slope, a_star_intercept = np.polyfit(a_star_num_nodes_visited_results, a_star_time_taken_ms_results, 1)
+a_star_line_of_best_fit = a_star_slope * np.array(a_star_num_nodes_visited_results) + a_star_intercept
+plt.plot(a_star_num_nodes_visited_results, a_star_line_of_best_fit, '-', label='A*')
 
-axises.text('')
+bfs_slope, bfs_intercept = np.polyfit(bfs_nodes_visited_results, bfs_time_taken_ms_results, 1)
+bfs_line_of_best_fit = bfs_slope * np.array(bfs_nodes_visited_results) + bfs_intercept
+plt.plot(bfs_nodes_visited_results, bfs_line_of_best_fit, '-', label='BFS')
+
+multithreaded_bfs_slope, multithreaded_bfs_intercept = np.polyfit(multithreaded_bfs_nodes_visited_results, multithreaded_bfs_time_taken_ms_results, 1)
+multithreaded_bfs_line_of_best_fit = multithreaded_bfs_slope * np.array(multithreaded_bfs_nodes_visited_results) + multithreaded_bfs_intercept
+plt.plot(multithreaded_bfs_nodes_visited_results, multithreaded_bfs_line_of_best_fit, '-', label='Multithreaded BFS')
+
+plt.figtext(0.0, -0.0, f'Avg Moves to Goal (A*): {(int)(a_star_num_moves_to_goal_avg + 0.5)}')
+plt.figtext(0.0, -0.1, f'Avg Moves to Goal (BFS): {(int)(bfs_num_moves_to_goal_avg + 0.5)}')
+plt.figtext(0.0, -0.2, f'Avg Moves to Goal (Multithreaded BFS): {(int)(multithreaded_bfs_num_moves_to_goal_avg + 0.5)}')
+
+plt.show()
